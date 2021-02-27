66452fe844a8122f5d53a38645523e1cfda2c28a
Max Wiens <maxawiens@gmail.com>
2021-02-26 20:27:49 -0600

    added enemy.cs

---
+++ Assets/Scripts/Entities/Enemies/Enemy.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour {
+	[SerializeField, NotNull] private NavMeshAgent _agent = default;
+
+	public void FollowPlayer(){
+		// if(_agent.SetDestination()){
+
+		// }else{
+		// 	b
+		// }
+	}
+}
