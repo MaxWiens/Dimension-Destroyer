ecb497bd71a3c349966095c41b781d63d00cf89b
Max Wiens <maxawiens@gmail.com>
2021-02-26 20:57:24 -0600

    added player transform reference in enemy script

--- Assets/Scripts/Entities/Enemies/Enemy.cs
+++ Assets/Scripts/Entities/Enemies/Enemy.cs
@@ -4,11 +4,27 @@ using UnityEngine.AI;
 public class Enemy : MonoBehaviour {
 	[SerializeField, NotNull] private NavMeshAgent _agent = default;
 
-	public void FollowPlayer(){
-		// if(_agent.SetDestination()){
+	private Transform _playerTransform = default;
 
-		// }else{
-		// 	b
-		// }
+	private void OnEnable() {
+		if(_playerTransform == null)
+			_playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+
+	}
+
+	private void OnDisable() {
+
+	}
+
+	private void FixedUpdate() {
+		FollowPlayer();
+	}
+
+	public void FollowPlayer(){
+		if(_agent.SetDestination(_playerTransform.position)){
+
+		}else{
+
+		}
 	}
 }
