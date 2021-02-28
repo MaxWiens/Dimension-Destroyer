11821e5a405957d64c0d07cae7c0c732cb8ce866
Max Wiens <maxawiens@gmail.com>
2021-02-28 10:19:08 -0600

    last minute additons

--- Assets/Scripts/Entities/EnemyAnimator.cs
+++ Assets/Scripts/Entities/EnemyAnimator.cs
@@ -20,15 +20,15 @@ public class EnemyAnimator : MonoBehaviour {
 		if(!_agent.isOnOffMeshLink && v != Vector3.zero){
 			Vector2 camForward = new Vector2(Camera.main.transform.forward.x, Camera.main.transform.forward.z);
 			float angle = Vector2.SignedAngle(camForward, new Vector2(v.x, v.z));
-			if(angle < 90 && angle > -90){
-				// is forward
+			// if(angle < 90 && angle > -90){
+			// 	// is forward
 				_animator.SetInteger("CurID", (int)EnemyAnimationState.moveForward);
-				isBack = false;
-			}else{
-				//is backward
-				_animator.SetInteger("CurID", (int)EnemyAnimationState.moveBackward);
-				isBack = true;
-			}
+			// 	isBack = false;
+			// }else{
+			// 	//is backward
+			// 	//_animator.SetInteger("CurID", (int)EnemyAnimationState.moveBackward);
+			// 	isBack = true;
+			// }
 
 			if(angle < 0){
 				_spriteRender.flipX = true;
@@ -36,11 +36,10 @@ public class EnemyAnimator : MonoBehaviour {
 				_spriteRender.flipX = false;
 			}
 		}else{
-			if(isBack){
-				_animator.SetInteger("CurID", (int)EnemyAnimationState.idleBackward);
-			}else{
+			// if(isBack){
+			// 	//_animator.SetInteger("CurID", (int)EnemyAnimationState.idleBackward);
+			// }else{
 				_animator.SetInteger("CurID", (int)EnemyAnimationState.idleForward);
-			}
 
 		 }
 		_prevPos = transform.position;
