f4446d735bf8958d10bab9721eba335a8369eca7
18ljones <ljones19992018@gmail.com>
2021-02-28 06:06:17 -0600

    More level stuff

--- Assets/FloatingScript.cs
+++ Assets/FloatingScript.cs
@@ -1,24 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
+#if UNITY_EDITOR
 using UnityEngine;
+using UnityEditor;
+#endif
 
-public class FloatingScript : MonoBehaviour
+public class TrianglePrimitive
 {
-    [SerializeField]
-    private Vector3 rotation;
-
-    [SerializeField]
-    private float rotationSpeed;
-
-    // Start is called before the first frame update
-    void Start()
+#if UNITY_EDITOR
+    private static Mesh CreateMesh()
     {
-        rotation.Normalize();
+        Vector3[] vertices = {
+             new Vector3(-0.5f, -0.5f, 0),
+             new Vector3(0.5f, -0.5f, 0),
+             new Vector3(0f, 0.5f, 0)
+         };
+
+        Vector2[] uv = {
+             new Vector2(0, 0),
+             new Vector2(1, 0),
+             new Vector2(0.5f, 1)
+         };
+
+        int[] triangles = { 0, 1, 2 };
+
+        var mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+        mesh.RecalculateTangents();
+        return mesh;
     }
 
-    // Update is called once per frame
-    void Update()
+    private static GameObject CreateObject()
     {
-        this.transform.Rotate(rotation * rotationSpeed * Time.deltaTime);
+        var obj = new GameObject("Triangle");
+        var mesh = CreateMesh();
+        var filter = obj.AddComponent<MeshFilter>();
+        var renderer = obj.AddComponent<MeshRenderer>();
+        var collider = obj.AddComponent<MeshCollider>();
+
+        filter.sharedMesh = mesh;
+        collider.sharedMesh = mesh;
+        renderer.sharedMaterial = AssetDatabase.GetBuiltinExtraResource<Material>("Default-Material.mat");
+
+        return obj;
     }
+
+    [MenuItem("GameObject/3D Object/Triangle", false, 0)]
+    public static void Create()
+    {
+        CreateObject();
+    }
+#endif
 }
