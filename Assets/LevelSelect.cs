1fb198046a92d79bc507aa233da8359e729d2f80
Max Wiens <maxawiens@gmail.com>
2021-02-28 10:56:09 -0600

    startedLevelSelect

---
+++ Assets/LevelSelect.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour
+{
+    void OnRealityDistoryion(){
+        SceneManager.LoadScene("RealBaseLevel2");
+    }
+
+    void OnInto(){
+        SceneManager.LoadScene("RealBaseLevel2");
+    }
+}
